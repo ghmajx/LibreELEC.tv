85a415e04b7709ab2d341c77288bb3b3f076d6d6
Omegamoon <omegamoon@gmail.com>
2017-02-20 17:13:08 +0100

    Package with Rockchip related tools

---
+++ packages/tools/rockchip-tools/package.mk
@@ -0,0 +1,28 @@
+#
+# Copyright © 2017 Omegamoon
+#
+
+PKG_NAME="rockchip-tools"
+PKG_VERSION="40c2d2d"
+PKG_ARCH="arm"
+
+PKG_LICENSE="GPL"
+PKG_SITE="http://rockchip.wikidot.com/"
+
+PKG_URL="https://github.com/omegamoon/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="$PKG_NAME-$PKG_VERSION*"
+
+PKG_DEPENDS_TARGET=""
+PKG_SECTION="tools"
+PKG_SHORTDESC="Rockchip tools"
+PKG_LONGDESC="Various Rockchip related tools"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+make_target() {
+  : # nothing
+}
+
+makeinstall_target() {
+  : # install from image script
+}
