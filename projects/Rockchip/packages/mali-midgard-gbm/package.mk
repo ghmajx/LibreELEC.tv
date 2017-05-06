b5c82a404749d7c3dde064d59cc34d5c10ad1855
Omegamoon <omegamoon@gmail.com>
2017-05-06 10:13:05 +0200

    Added utgard driver for rk3328-box

--- projects/Rockchip/packages/mali-midgard-gbm/package.mk
+++ projects/Rockchip/packages/mali-midgard-gbm/package.mk
@@ -18,7 +18,7 @@
 
 PKG_NAME="mali-midgard-gbm"
 PKG_VERSION="863a77f"
-PKG_ARCH="arm"
+PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://developer.arm.com/products/software/mali-drivers/user-space"
 PKG_URL="https://github.com/rockchip-linux/libmali/archive/$PKG_VERSION.tar.gz"
@@ -50,8 +50,15 @@ makeinstall_target() {
     cp -PRv include/gbm.h $SYSROOT_PREFIX/usr/include
 
   mkdir -p $SYSROOT_PREFIX/usr/lib
-    cp -PRv lib/arm-linux-gnueabihf/libmali-midgard-r13p0-r0p0-gbm.so $SYSROOT_PREFIX/usr/lib
-    ln -sf libmali-midgard-r13p0-r0p0-gbm.so $SYSROOT_PREFIX/usr/lib/libmali.so
+    if [ "$DEVICE" = "TinkerBoard" ]; then
+      cp -PRv lib/arm-linux-gnueabihf/libmali-midgard-r13p0-r0p0-gbm.so $SYSROOT_PREFIX/usr/lib
+      ln -sf libmali-midgard-r13p0-r0p0-gbm.so $SYSROOT_PREFIX/usr/lib/libmali.so
+    elif [ "$DEVICE" = "rk3328-box" ]; then
+# should be: aarch64-linux-gnu/libmali-utgard-2th-r7p0-gbm.so
+      cp -PRv lib/arm-linux-gnueabihf/lib32mali-utgard-2th-r7p0-gbm.so $SYSROOT_PREFIX/usr/lib
+      ln -sf lib32mali-utgard-2th-r7p0-gbm.so $SYSROOT_PREFIX/usr/lib/libmali.so
+    fi
+    
     ln -sf libmali.so $SYSROOT_PREFIX/usr/lib/libEGL.so
     ln -sf libmali.so $SYSROOT_PREFIX/usr/lib/libGLESv2.so
     ln -sf libmali.so $SYSROOT_PREFIX/usr/lib/libgbm.so
@@ -59,9 +66,19 @@ makeinstall_target() {
   mkdir -p $SYSROOT_PREFIX/usr/lib/pkgconfig
     cp -PRv $PKG_DIR/pkgconfig/*.pc $SYSROOT_PREFIX/usr/lib/pkgconfig
 
+echo "### >>> Omegamoon: Installing Mali in $INSTALL/usr/lib; device is $DEVICE"
   mkdir -p $INSTALL/usr/lib
-    cp -PRv lib/arm-linux-gnueabihf/libmali-midgard-r13p0-r0p0-gbm.so $INSTALL/usr/lib
-    ln -sf libmali-midgard-r13p0-r0p0-gbm.so $INSTALL/usr/lib/libmali.so
+    if [ "$DEVICE" = "TinkerBoard" ]; then
+echo "### >>> Omegamoon: Installing Midgard Mali for $DEVICE"
+      cp -PRv lib/arm-linux-gnueabihf/libmali-midgard-r13p0-r0p0-gbm.so $INSTALL/usr/lib
+      ln -sf libmali-midgard-r13p0-r0p0-gbm.so $INSTALL/usr/lib/libmali.so
+    elif [ "$DEVICE" = "rk3328-box" ]; then
+echo "### >>> Omegamoon: Installing Utgard Mali for $DEVICE"
+# should be: aarch64-linux-gnu/libmali-utgard-2th-r7p0-gbm.so
+      cp -PRv lib/arm-linux-gnueabihf/lib32mali-utgard-2th-r7p0-gbm.so $INSTALL/usr/lib
+      ln -sf lib32mali-utgard-2th-r7p0-gbm.so $INSTALL/usr/lib/libmali.so
+    fi
+
     ln -sf libmali.so $INSTALL/usr/lib/libEGL.so
     ln -sf libmali.so $INSTALL/usr/lib/libGLESv2.so
     ln -sf libmali.so $INSTALL/usr/lib/libgbm.so
